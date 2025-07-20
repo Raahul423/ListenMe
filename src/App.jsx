e1b6b61fd6f4d18e091303e5c74b7f8cfae386be
Raahul423 <rpal24480@gmail.com>
2025-07-20 15:43:25 +0530

    fix issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import React, { useEffect, useState } from "react"
-import { Route, Router, Routes } from "react-router-dom"
+import React from "react"
+import { Route, Routes } from "react-router-dom"
 import Home from "./Pages/Home"
 import Favourites from "./Pages/Favourites"
 import Library from "./Pages/Library"
-import Trending from "./Pages/Trending"
 import { Toaster } from "react-hot-toast"
 import Sidebar from "./Components/Sidebar"
-import Feed from "./Pages/Feed"
 import MobileSidebar from "./Components/MobileSide"
 
-
-
-
 function App() {
-
-  return ( 
+  return (
     <>
-    {<Sidebar/>}
-    <MobileSidebar/>
-    <Toaster/>
-     <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="Favourites" element={<Favourites/>} />
-      <Route path="Library" element={<Library/>} />
-      <Route path="Feed" element={<Feed/>} />
-      <Route path="Trending" element={<Trending/>} />
-    </Routes>
+      <Sidebar />
+      <MobileSidebar />
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Favourites" element={<Favourites />} />
+        <Route path="/Library" element={<Library />} />
+      </Routes>
     </>
-   
-  
   )
 }
 
